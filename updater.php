2911eb8895d9b4732c53a4504c11ce0ff50af20c
DanZ Digital Designs <56734927+danzdigital@users.noreply.github.com>
2021-12-01 10:28:28 +0000

    Update updater.php

--- updater.php
+++ updater.php
@@ -122,12 +122,6 @@ class ChurchSuite_Events_Updater {
 				$plugin = array(
 					'name'				=> $this->plugin["Name"],
 					'slug'				=> $this->basename,
-					'requires'					=> '3.3',
-					'tested'						=> '4.4.1',
-					'rating'						=> '100.0',
-					'num_ratings'				=> '10823',
-					'downloaded'				=> '14249',
-					'added'							=> '2016-01-05',
 					'version'			=> $this->github_response['tag_name'],
 					'author'			=> $this->plugin["AuthorName"],
 					'author_profile'	=> $this->plugin["AuthorURI"],
